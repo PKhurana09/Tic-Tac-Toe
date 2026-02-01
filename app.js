766b03fee425e182de1fa8710880a03220538ed7
Pranav Khurana <pranavkhurana69@gmail.com>
2026-02-01 13:27:23 +0530

    Basic Layout and designing completed. Need to finish the game login

---
+++ app.js
@@ -0,0 +1,36 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let turn0 = true; //playerX, player0
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach( (box) => {
+    box.addEventListener("click", () =>{
+        console.log('Box was clicked');
+        if(turn0){ // If player 0 turn
+            box.innerText = "O";
+            turn0 = false;
+        }else{ // If player X turn
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    })
+})
+
+const checkWinner = () =>{
+    for(pattern of winPatterns){
+        console.log(pattern);
+    }
+}
